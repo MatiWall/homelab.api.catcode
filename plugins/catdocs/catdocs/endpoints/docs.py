5b535099ce51a72452db6e5cb55569cc8149b69d
Matias Wallenius <MatiWall@live.dk>
2024-03-13 20:54:52 +0100

    initial docs working

--- plugins/catdocs/catdocs/endpoints/docs.py
+++ plugins/catdocs/catdocs/endpoints/docs.py
@@ -3,9 +3,19 @@ import settings
 
 router = APIRouter(prefix='/docs')
 
+@router.get('/static/{system}/{application}/{deployableUnit}')
+def return_static_css(system, application, deployableUnit):
 
+    file_path = settings.BASE_DIR / f'builds/{system}.{application}.{deployableUnit}/site/assets/stylesheets/main.7e359304.min.css'
+    try:
+        with file_path.open('r') as file:
+            html_content = file.read()
+
+        return Response(content=html_content, media_type="text/css")
+    except FileNotFoundError:
+        return {"message": "Documentation file not found"}
 @router.get('/{system}/{application}/{deployableUnit}/{path:path}')
-def return_docs(system, application, deployableUnit, path):
+def return_html_docs(system, application, deployableUnit, path):
 
     file_path = settings.BASE_DIR / f'builds/{system}.{application}.{deployableUnit}/site/{path}/index.html'
     try:
@@ -15,3 +25,4 @@ def return_docs(system, application, deployableUnit, path):
         return Response(content=html_content, media_type="text/html")
     except FileNotFoundError:
         return {"message": "Documentation file not found"}
+
