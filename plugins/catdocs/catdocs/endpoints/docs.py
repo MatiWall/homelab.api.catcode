3b04281dbf47617da7768fc5392b7041fe06b2d6
Matias Wallenius <MatiWall@live.dk>
2024-03-13 20:26:04 +0100

    page retrival from url working

--- plugins/catdocs/catdocs/endpoints/docs.py
+++ plugins/catdocs/catdocs/endpoints/docs.py
@@ -4,10 +4,10 @@ import settings
 router = APIRouter(prefix='/docs')
 
 
-@router.get('/{system}/{application}/{deployableUnit}')
-def return_docs(system, application, deployableUnit):
+@router.get('/{system}/{application}/{deployableUnit}/{path:path}')
+def return_docs(system, application, deployableUnit, path):
 
-    file_path = settings.BASE_DIR / f'builds/{system}.{application}.{deployableUnit}/site/index.html'
+    file_path = settings.BASE_DIR / f'builds/{system}.{application}.{deployableUnit}/site/{path}/index.html'
     try:
         with file_path.open('r') as file:
             html_content = file.read()
